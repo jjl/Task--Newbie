a7922bb009c1acf6010f66dc5f95ee68aae1e21c
James Laver <james@jameslaver.com>
2010-02-28 15:32:41 +0200

    Started module

---
+++ lib/Task/Newbie.pm
@@ -0,0 +1,80 @@
+package Task::Newbie;
+
+our $VERSION = 0.001;
+
+1;
+__END__
+
+=head1 NAME
+
+Task::Newbie - Make life easy for newbies
+
+=head1 VERSION
+
+Version 0.001
+
+=head1 SYNOPSIS
+
+ wget -O- http://xrl.us/cpanm | perl - Task::Newbie
+
+=head1 WHAT THIS MODULE DOES
+
+At the minute, not a lot, but here's what it will do:
+
+=over 4
+
+=item configure more sane defaults for CPAN.pm and CPANPLUS.pm
+
+=item install the basic modules that a newbie is likely to want/need
+
+=cut
+
+=head1 AUTHOR
+
+James Laver, C<< <cpan at jameslaver.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-task-newbie at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-Newbie>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Task::Newbie
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Newbie>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-Newbie>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-Newbie>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-Newbie/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 James Laver, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
